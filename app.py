99d2dbcdcd2eaca2ae49ac1f8698df77a5c1343d
Shasass12 <shachsasson@gmail.com>
2022-03-09 23:26:06 +0200

    added file for heroku

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import flask
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def app():  # put application's code here
+def home():  # put application's code here
     return flask.render_template('index.html')
 
 
