016eac1a060638062409966bece226c7382409b7
Shasass12 <shachsasson@gmail.com>
2022-03-13 20:51:18 +0200

    ass css to project

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def home():  # put application's code here
     return flask.render_template('index.html')
 
+#@app.route('/goSomeWhere')
+#def some_name():  # put application's code here
+#    return flask.render_template('other_class.html')
+
+
 
 if __name__ == '__main__':
     app.run()
