af90d729790dacef657279b928f088172285669f
Shasass12 <shachsasson@gmail.com>
2022-03-23 22:33:38 +0200

    project update

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import flask
-from flask import Flask, render_template
+from flask import Flask
+from pages.home.home import home
+from pages.sheelon.sheelon import sheelon
+from pages.sheelon2.sheelon2 import sheelon2
+from pages.recommendation.recommendation import recommendation
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def home():  # put application's code here
-    return flask.render_template('index.html')
-
-#@app.route('/goSomeWhere')
-#def some_name():  # put application's code here
-#    return flask.render_template('other_class.html')
-
+app.register_blueprint(sheelon)
+app.register_blueprint(sheelon2)
+app.register_blueprint(home)
+app.register_blueprint(recommendation)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1', port=5000, threaded=True)
